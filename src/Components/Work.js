fd278fc9c519f5f52e90c8f02c1386967692be5b
Natalee Amhaz <natalee_amhaz@brown.edu>
2022-12-14 09:14:44 -0500

    j

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -43,8 +43,8 @@ const Work = () => {
           class="col-8 col-sm-6"
           id="box"
         >
-          <a href="/fleek">
-          <img src={require("../new.png")} width="500" height="250"></img>
+          <a href="/again/fleek">
+            <img src={require("../new.png")} width="500" height="250"></img>
           </a>
           {isHovering && (
             <div>
@@ -62,7 +62,7 @@ const Work = () => {
           class="col-8 col-sm-6"
           id="box"
         >
-          <a href="/eye">
+          <a href="/again/eye">
             <img src={require("../test.png")} width="500" height="250"></img>
           </a>
           {isHovering && (
@@ -78,7 +78,7 @@ const Work = () => {
           class="col-8 col-sm-6"
           id="box"
         >
-          <a href="/pers">
+          <a href="/again/pers">
             <div>
               <img src={require("../pers.png")} width="500" height="250"></img>
             </div>
