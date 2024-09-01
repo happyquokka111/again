3e614f2cbb372ec04d6d029e9aa0844d2d9845b3
happyquokka111 <natalee_amhaz@brown.edu>
2024-09-01 12:55:41 -0400

    final changes

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,18 +1,24 @@
 import React from "react";
 import "./About.css";
+
 const About = () => {
-  
   return (
     <div className="about">
-      <h1>About Me</h1>
-      <div className="image-container">  <img className = "profile-image" id="im" src={require("../me.png")} ></img></div>
-      <p>
-        I am a Brown University alum with bachelor degrees in Computer Science and
-        Contemplative Studies (a major that consists of neuroscience, psychology, and philosophy courses). I am a NY based software engineer passionate about technology and the impact it has on people's lives.
-        <br/>
-         Always on the look at for some cool projects, let me know of any!
-      </p>
+      <h1 className="title">About Me</h1>
+      <div className="content">
+        <div className="image-container">
+          <img className="profile-image" src={require("../me.png")} alt="Profile" />
+        </div>
+        <div className="about-text">
+          <p>
+            I am a Brown University alum with bachelor degrees in Computer Science and
+            Contemplative Studies (a major that consists of neuroscience, psychology, and philosophy courses). I am a NY based software engineer passionate about technology and the impact it has on people's lives.
+            <br />
+            Always on the lookout for some cool projects, let me know of any!
+          </p>
+        </div>
+      </div>
     </div>
   );
-};
+}
 export default About;
