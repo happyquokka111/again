64f6e5b41ccf3e997bd1a457febdd929723a3a23
happyquokka111 <natalee_amhaz@brown.edu>
2024-09-01 22:42:35 -0400

    ix

--- src/Components/Kara.js
+++ src/Components/Kara.js
@@ -8,7 +8,7 @@ const Kara = () => {
   
     <div className="Kara">
       <div className="top">
-        {<img className="name" src={`${process.env.PUBLIC_URL}${require("../logo2.png")}`} width="7%"></img>}
+        {<img className="name" src={require("../logo2.png")} width="7%"></img>}
 
         <div className="nav">
           <ul className="navbar-nav">
